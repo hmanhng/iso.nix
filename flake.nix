f11ad3763875996ef7f2bd6c848e5c1ad22a7502
Hmanhng <hmanhng@icloud.com>
2023-06-02 13:27:02 +0700

    mini iso with flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Flake for building NixOS images";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = { self, ... }@inputs:
+    let
+      nodes = {
+        "my-new-pc" = { system = "x86_64-linux"; format = "isoImage"; };
+      };
+
+      osConfigs = builtins.mapAttrs
+        (name: value: inputs.nixpkgs.lib.nixosSystem {
+          system = value.system;
+          modules = [
+            (./. + "/hosts/${name}.nix")
+          ];
+          specialArgs = {
+            inherit inputs;
+          };
+        })
+        nodes;
+
+      images = builtins.mapAttrs
+        (name: value: value.config.system.build.${ nodes.${name}.format })
+        osConfigs;
+    in
+    {
+      nixosConfigurations = ({ } // osConfigs);
+    } // inputs.utils.lib.eachDefaultSystem
+      (system:
+        {
+          packages.image = images;
+        }
+      );
+}
